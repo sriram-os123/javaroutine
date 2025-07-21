ada92d526a5fa7b491a394804484f9653f0d0247
sriram os123 <118521565+sriram-os123@users.noreply.github.com>
2025-07-21 22:47:58 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+//my first program to test how code works to deal with syntax
+
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+       System.out.println("this is my code");
+
+       int a=45;
+       int b=67;
+       int c=98;
+       int sum=a+b+c;
+       System.out.println("this is sum of a,b,c :"+sum);
+
+    }
+}
